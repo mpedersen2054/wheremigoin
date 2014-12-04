1dc88d0318fe2c9fb3fa8f5afe938d8f51e046ea
Matt Pedersen <mpedersen2054@gmail.com>
2014-12-03 21:19:11 -0600

    add working getCords method to locationObj that uses the addr city and state to request info from google geocoding api to aquire lat/long

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 $(function() {
 
-    var loc1 = new locationObj('1331 W. Windhill Dr.',
+    var loc1 = new locationObj('1331 W Windhill Dr',
                                'Palatine',
                                'IL',
                                '60067');
@@ -13,12 +13,22 @@ $(function() {
         this.city = city;
         this.state = state;
         this.zip = zip;
-        this.lat = ;
-        this.long = ;
+        // this.lat = ;
+        // this.long = ;
 
         // ?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=KEY
+        // https://maps.googleapis.com/maps/api/geocode/json?address=1331+W+Windhill+Dr,+Palatine+Area,+IL
         this.getCords = function() {
-            var url =
+            var key = 'AIzaSyAYrG38he5q3MzcsxyAYC_uWsSFy5SSG7w',
+                url = '';
+
+            url+='https://maps.googleapis.com/maps/api/geocode/json';
+            url+='?address='+this.addr.split(' ').join('+');
+            url+=',+'+this.city.split(' ').join('+');
+            url+=',+'+this.state;
+            url+='&key='+key;
+
+            console.log(url)
         }
     }
 
