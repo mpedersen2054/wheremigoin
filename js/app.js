36cb3bbe0c76d722c2f70550082311216a752730
Matt Pedersen <mpedersen2054@gmail.com>
2014-12-07 17:52:47 -0600

    add getJSON call to .getCords

--- js/app.js
+++ js/app.js
@@ -13,8 +13,6 @@ $(function() {
         this.city = city;
         this.state = state;
         this.zip = zip;
-        // this.lat = ;
-        // this.long = ;
 
         // ?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=KEY
         // https://maps.googleapis.com/maps/api/geocode/json?address=1331+W+Windhill+Dr,+Palatine+Area,+IL
@@ -28,7 +26,13 @@ $(function() {
             url+=',+'+this.state;
             url+='&key='+key;
 
-            console.log(url)
+            $.getJSON(url, function(data) {
+                var locLat = data.results[0].geometry.location.lat,
+                    locLng = data.results[0].geometry.location.lng
+
+                var cords = [locLat, locLng];
+                console.log(cords)
+            })
         }
     }
 
