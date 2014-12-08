db99ee6010fcb5d20de6f685401f6cc52b27d207
Matt Pedersen <mpedersen2054@gmail.com>
2014-12-08 15:36:05 -0600

    random styling adjustments, add click event handler to collect all geo info to be converted into geocords

--- js/app.js
+++ js/app.js
@@ -2,46 +2,78 @@ $(function() {
 
     $('a.search').on('click', function(event) {
         event.preventDefault();
+        var street = $('#street-input'),
+            city = $('#city-input'),
+            state = $('#state-input'),
+            zip = $('#zip-input'),
+            address = [street,city,state,zip]
 
         $('.top-nav').css('margin-top', '0')
-    })
 
-    var loc1 = new locationObj('1331 W Windhill Dr',
-                               'Palatine',
-                               'IL',
-                               '60067');
+        CLIENT.init(address)
+    });
+    
+    var loc1 = ['1331 W Windhill Dr', 'Palatine', 'IL', '60067']
 
-    loc1.geoCords()
+    var CLIENT = CLIENT || {
+
+        init: function(address) {
+            CLIENT.street = address[0];
+            CLIENT.city = address[1];
+            CLIENT.state = address[2];
+            CLIENT.zip = address[3];
+            var addr = address;
+
+            CLIENT.getCords(addr)
+        },
+
+        getCords: function(address) {
+            var key = 'AIzaSyAYrG38he5q3MzcsxyAYC_uWsSFy5SSG7w',
+            url = '';
+
+            url+='https://maps.googleapis.com/maps/api/geocode/json';
+            url+='?address='+address[0].split(' ').join('+');
+            url+=',+'+address[1].split(' ').join('+');
+            url+=',+'+address[2];
+            url+='&key='+key;
+
+            console.log(url)
+
+
+            $.getJSON(url, function(data) {
+                var lat = data.results[0].geometry.location.lat,
+                    lng = data.results[0].geometry.location.lng
+
+                var cords = [lat, lng];
+                // console.log(cords)
+                CLIENT.setCords(cords)
+            })
+        },
+
+        setCords: function(cords) {
+            console.log(cords[1])
+            CLIENT.cords = cords;
+        }
+
+    }
+
+    
+    CLIENT.init(loc1)
+    console.log(CLIENT.cords)
+    
 
     function locationObj(addr,city,state,zip) {
         this.addr = addr;
         this.city = city;
         this.state = state;
         this.zip = zip;
-        this.geoCords = function() {
-            return this.getCords
-        }
 
         // ?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=KEY
         // https://maps.googleapis.com/maps/api/geocode/json?address=1331+W+Windhill+Dr,+Palatine+Area,+IL
-        this.getCords = function() {
-            var key = 'AIzaSyAYrG38he5q3MzcsxyAYC_uWsSFy5SSG7w',
-                url = '';
+        
 
-            url+='https://maps.googleapis.com/maps/api/geocode/json';
-            url+='?address='+this.addr.split(' ').join('+');
-            url+=',+'+this.city.split(' ').join('+');
-            url+=',+'+this.state;
-            url+='&key='+key;
+        // console.log(locLat)
 
-            $.getJSON(url, function(data) {
-                var locLat = data.results[0].geometry.location.lat,
-                    locLng = data.results[0].geometry.location.lng
-
-                var cords = [locLat, locLng];
-                console.log(cords)
-            })
-        }
     }
 
 })();
