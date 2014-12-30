90532afcd8e99108d55b38df87f7a172208cd305
Matt Pedersen <mpedersen2054@gmail.com>
2014-12-29 20:52:40 -0600

    create EVENTS obj to store the click functions

--- js/app.js
+++ js/app.js
@@ -1,121 +1,120 @@
-$(function() {
-    // from js/location_form_data.js 
-    $('#state-input').html(usStateData);
-
-    $('a.search').on('click', function(event) {
-        $('.top-nav').css('margin-top', '0');
-    })
-
-    $('#location-form').on('submit', function(event) {
-        event.preventDefault();
-        var street = $('#street-input').val(),
-            city = $('#city-input').val(),
-            state = $('#state-input').val(),
-            zip = $('#zip-input').val(),
-            address = [street,city,state,zip]
-
-        console.log(address)
-
-        CLIENT.init(address);
-    });
+(function($, document) {
     
-    var loc1 = ['141 W Jackson Blvd', 'Chicago', 'IL', '60604']
+var loc1 = ['141 W Jackson Blvd', 'Chicago', 'IL', '60604']
 
-    var CLIENT = CLIENT || {
+var CLIENT = CLIENT || {
 
-        init: function(address) {
-            CLIENT.street = address[0];
-            CLIENT.city = address[1];
-            CLIENT.state = address[2];
-            CLIENT.zip = address[3];
-            var addr = address;
+    init: function(address) {
+        CLIENT.street = address[0];
+        CLIENT.city = address[1];
+        CLIENT.state = address[2];
+        CLIENT.zip = address[3];
+        var addr = address;
 
-            CLIENT.getCords(addr)
-        },
+        // turn events on
+        EVENTS.searchDropdown();
+        EVENTS.searchFormSub();
 
-        getCords: function(address) {
-            var key = 'AIzaSyAYrG38he5q3MzcsxyAYC_uWsSFy5SSG7w',
-                url = '';
+        CLIENT.getCords(addr);
+    },
 
-            url+='https://maps.googleapis.com/maps/api/geocode/json';
-            url+='?address='+address[0].split(' ').join('+');
-            url+=',+'+address[1].split(' ').join('+');
-            url+=',+'+address[2];
-            url+='&key='+key;
+    getCords: function(address) { // from google maps api
+        var key = 'AIzaSyAYrG38he5q3MzcsxyAYC_uWsSFy5SSG7w',
+            url = '';
 
-            $.getJSON(url, function(data) {
-                var lat = data.results[0].geometry.location.lat,
-                    lng = data.results[0].geometry.location.lng,
-                    cords = [lat, lng];
+        url+='https://maps.googleapis.com/maps/api/geocode/json';
+        url+='?address='+address[0].split(' ').join('+');
+        url+=',+'+address[1].split(' ').join('+');
+        url+=',+'+address[2];
+        url+='&key='+key;
 
-                $('.loc-img').find('img').remove();
-                $('.loc-img-title').html('')
-                                   .append('<a href="#">'+CLIENT.city+'</a>')
-                $('.loc-img').append('<img src="https://maps.googleapis.com/maps/api/staticmap?center='+lat+','+lng+'&zoom=11&size=1600x1200" width="940px" height="560px">')
+        $.getJSON(url, function(data) {
+            var lat = data.results[0].geometry.location.lat,
+                lng = data.results[0].geometry.location.lng,
+                cords = [lat, lng];
 
-                CLIENT.requestLqi(cords)
-                CLIENT.requestDemographics(cords)
-            })
-        },
+            $('.loc-img').find('img').remove();
+            $('.loc-img-title').html('')
+                               .append('<a href="#">'+CLIENT.city+'</a>')
+            $('.loc-img').append('<img src="https://maps.googleapis.com/maps/api/staticmap?center='+lat+','+lng+'&zoom=11&size=1600x1200" width="940px" height="560px">')
 
-        requestLqi: function(cords) {
-            var lat = cords[0],
-                lng = cords[1],
-                cordsForUrl = lat+','+lng
+            CLIENT.requestLqi(cords)
+            CLIENT.requestDemographics(cords)
+        })
+    },
 
-            url = 'http://api.placeilive.com/v1/houses/search?callback=?';
+    requestLqi: function(cords) {
+        var lat = cords[0],
+            lng = cords[1],
+            cordsForUrl = lat+','+lng
 
-            var jqxhr = $.getJSON(url, { ll: cordsForUrl }, function (data) {
-                console.log(data)
-            })
-            console.log(jqxhr)
-            // .done(function(data) {
-            //     console.log(data)
-            // })
-        },
+        url = 'http://api.placeilive.com/v1/houses/search?callback=?';
 
-        // http://www.broadbandmap.gov/broadbandmap/demographic/2012/coordinates?latitude=42.456&longitude=-74.987&format=json
-        requestDemographics: function(cords) {
-            var lat = cords[0],
-                lng = cords[1],
-                url = 'http://www.broadbandmap.gov/broadbandmap/demographic/2012/coordinates?callback=?';
+        console.log('hello')
+    },
 
-            var params = {
-                latitude: lat,
-                longitude: lng,
-                format: 'jsonp'
+    // http://www.broadbandmap.gov/broadbandmap/demographic/2012/coordinates?latitude=42.456&longitude=-74.987&format=json
+    requestDemographics: function(cords) {
+        var lat = cords[0],
+            lng = cords[1],
+            url = 'http://www.broadbandmap.gov/broadbandmap/demographic/2012/coordinates?callback=?';
+
+        var params = {
+            latitude: lat,
+            longitude: lng,
+            format: 'jsonp'
+        }
+
+        $.getJSON(url, params, function(data) {
+            insertData(data.Results)
+        });
+
+        function insertData(results) {
+            var college = round('educationBachelorOrGreater'),
+                hschool = round('educationHighSchoolGraduate'),
+                medianIncome = results['medianIncome'],
+                incomeBelowPov = round('incomeBelowPoverty'),
+                incomeLessThan25 = round('incomeLessThan25'),
+                income25t50 = round('incomeBetween25to50'),
+                income50t100 = round('incomeBetween50to100'),
+                income100t200 = round('incomeBetween100to200');
+
+            function round(resultsProp) { // uses data from results
+                return Math.round(results[resultsProp] * 100)
             }
 
-            $.getJSON(url, params, function(data) {
-                insertData(data.Results)
-            })
-
-            function insertData(results) {
-                var college = round('educationBachelorOrGreater'),
-                    hschool = round('educationHighSchoolGraduate'),
-                    medianIncome = results['medianIncome'],
-                    incomeBelowPov = round('incomeBelowPoverty'),
-                    incomeLessThan25 = round('incomeLessThan25'),
-                    income25t50 = round('incomeBetween25to50'),
-                    income50t100 = round('incomeBetween50to100'),
-                    income100t200 = round('incomeBetween100to200')
-
-                function round(resultsProp) {
-                    return Math.round(results[resultsProp] * 100)
-                }
-
-                $('.gc').text(college + '%');
-                $('.ghs').text(hschool + '%');
-                $('.median').text('$' + medianIncome);
-                $('.ibp').text(incomeBelowPov + '%');
-                $('.ilt25').text(incomeLessThan25 + '%');
-                $('.i25-50').text(income25t50 + '%');
-                $('.i50-100').text(income50t100 + '%');
-                $('.i100-200').text(income100t200 + '%');
-            }
+            $('.gc').text(college + '%');
+            $('.ghs').text(hschool + '%');
+            $('.median').text('$' + medianIncome);
+            $('.ibp').text(incomeBelowPov + '%');
+            $('.ilt25').text(incomeLessThan25 + '%');
+            $('.i25-50').text(income25t50 + '%');
+            $('.i50-100').text(income50t100 + '%');
+            $('.i100-200').text(income100t200 + '%');
         }
     }
-    
-    CLIENT.init(loc1)
+}
 
-})();
+var EVENTS = EVENTS || {
+    searchDropdown: function() {
+        $('a.search').on('click', function(event) {
+            $('.top-nav').css('margin-top', '0');
+        });
+    },
+    searchFormSub: function() {
+        $('#location-form').on('submit', function(event) {
+            event.preventDefault();
+            var street = $('#street-input').val(),
+                city = $('#city-input').val(),
+                state = $('#state-input').val(),
+                zip = $('#zip-input').val(),
+                address = [street,city,state,zip];
+
+            CLIENT.init(address);
+        });
+    }
+}
+
+CLIENT.init(loc1);
+
+})(jQuery, document);
